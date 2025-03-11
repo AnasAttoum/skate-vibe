f85c9c9c3c4639f1d83eee18e0f23d155b9f27a6
Anas Attoum <anasattoum.12321@gmail.com>
2025-03-11 16:12:15 +0300

    Update on hero section

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import React from "react";
 
 type Props = {
-  comp?: "h1" | "h2";
+  comp?: "h1" | "h2" | "h3" | "h4";
   children: React.ReactNode;
   className?: string;
 };
@@ -17,7 +17,9 @@ export default function Heading({
       className={clsx(
         "font-sans uppercase",
         Comp === "h1" && "text-2xl md:text-3xl lg:text-4xl font-extrabold",
-        Comp === "h2" && "text-md md:text-lg lg:text-2xl font-bold",
+        Comp === "h2" && "text-lg md:text-2xl font-bold",
+        Comp === "h3" && "text-md md:text-lg lg:text-2xl font-bold",
+        Comp === "h4" && "text-sm md:text-md lg:text-lg font-bold",
         className
       )}
     >
