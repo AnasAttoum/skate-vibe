3289e5710363100bfd392815162390bbc79c1aac
Anas Attoum <anasattoum.12321@gmail.com>
2025-03-12 17:12:58 +0300

    Fix bug

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import React from 'react'
-import Lottie from 'lottie-react'
-import bgAnimation from "@/assets/background/bg-logo.json";
 
 export default function Background() {
   return (
-    <Lottie animationData={bgAnimation} loop={true} className='opacity-20 absolute inset-0'/>
+    <div></div>
   )
 }
