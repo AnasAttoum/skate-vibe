76ac7d5520f51e01bf2b4105ae3a17ca9876ab67
Anas Attoum <anasattoum.12321@gmail.com>
2025-03-16 21:46:54 +0300

    Complete about sections

--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -1,34 +1,36 @@
+import clsx from "clsx";
+
 import Bounded from "@/components/bounded";
 import Heading from "@/components/heading";
 import ParallaxImages from "@/components/parallax-images";
-import clsx from "clsx";
-
-type Props = {
-  title: string;
-  description: string;
-  foregroundImage: string;
-  bg: string;
-  changeOrder?: boolean;
-};
+import type { About } from "@/types/about";
 
 export default function About({
+  id = 0,
   title,
   description,
   foregroundImage,
   bg,
-  changeOrder = false
-}: Props) {
+  changeOrder = false,
+  isLast,
+}: About & { isLast: boolean }) {
   return (
-    <Bounded className={clsx("text-white", bg)}>
+    <Bounded
+      className={clsx(!isLast ? "sticky" : "relative", "text-white", bg)}
+      style={!isLast ? { top: id * 2 - 2 + "rem" }:{}}
+    >
       <div className="grid grid-cols-1 md:grid-cols-2 items-center my-0 md:my-10">
         <div className={clsx(changeOrder && "order-2")}>
-          <Heading className="my-10 leading-10">{title}</Heading>
+          <Heading className="my-5 md:my-10 leading-10">{title}</Heading>
           <Heading comp="h4" className="max-w-md leading-8">
             {description}
           </Heading>
         </div>
 
-        <ParallaxImages foregroundImage={foregroundImage} alt={foregroundImage.split("/").pop()?.split('.').shift() || 'icon'} />
+        <ParallaxImages
+          foregroundImage={foregroundImage}
+          alt={foregroundImage.split("/").pop()?.split(".").shift() || "icon"}
+        />
       </div>
     </Bounded>
   );
---
+++ src/types/about.ts
@@ -0,0 +1,8 @@
+export type About = {
+  id: number;
+  title: string;
+  description: string;
+  foregroundImage: string;
+  bg: string;
+  changeOrder?: boolean;
+};
