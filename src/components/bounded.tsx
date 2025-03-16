f22980f207102581720c2f6719051544121790d9
Anas Attoum <anasattoum.12321@gmail.com>
2025-03-16 21:58:12 +0300

    Specify type instaed of any

--- src/components/bounded.tsx
+++ src/components/bounded.tsx
@@ -1,16 +1,16 @@
 import clsx from "clsx";
-import React from "react"
+import React from "react";
 
 type Props = {
   children: React.ReactNode;
   className?: string;
-  style?: any;
-}
+  style?: { [key: string]: string };
+};
 
-export default function Bounded({children, className, style={}}:Props) {
+export default function Bounded({ children, className, style = {} }: Props) {
   return (
     <div className={clsx("px-6 py-10 bg-texture", className)} style={style}>
       <div className="mx-auto w-full max-w-6xl">{children}</div>
     </div>
-  )
+  );
 }
