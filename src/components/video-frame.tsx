eeba3e34350b86fe269b0e15cef7c8dfe85cca6f
Anas Attoum <anasattoum.12321@gmail.com>
2025-03-17 19:53:48 +0300

    Add observer to video

--- src/components/video-frame.tsx
+++ src/components/video-frame.tsx
@@ -1,7 +1,41 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
 type Props = {
   src?: string;
 };
 
-export default function VideoFrame({src="/assets/video/skateboard.mp4"}: Props) {
-  return <video src={src} autoPlay loop muted />;
+export default function VideoFrame({
+  src = "/assets/video/skateboard.mp4",
+}: Props) {
+  const myVideo = useRef<HTMLDivElement>(null);
+  const [isInView, setIsInView] = useState(false);
+
+  useEffect(() => {
+    const currentVideo = myVideo.current;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) setIsInView(true);
+      },
+      { threshold: 0, rootMargin: "1500px" }
+    );
+
+    if (currentVideo) {
+      observer.observe(currentVideo);
+    }
+
+    return () => {
+      if (currentVideo) {
+        observer.unobserve(currentVideo);
+      }
+    };
+  });
+
+  return (
+    <div ref={myVideo}>
+      {isInView && <video src={src} autoPlay loop muted />}
+    </div>
+  );
 }
