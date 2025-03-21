d47a0f3dd5e17673319bd88f2410a922ee9e0d42
Anas Attoum <anasattoum.12321@gmail.com>
2025-03-21 20:17:51 +0300

    Update footer

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -1,24 +1,18 @@
+import Bounded from "@/components/bounded";
 import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className="bg-zinc-900 bg-texture h-screen text-white">
-      <div className="w-full h-4/5 aspect-video z-10 relative">
-        <Image
-          src="/assets/background/paint-skateboard.jpg"
-          alt="Paint skateboard"
-          fill
-        />
+    <Bounded className="bg-zinc-800 bg-texture text-white">
+      <div className="flex max-md:flex-col items-center md:justify-between">
         <Image
           src="/assets/icons/skate-vibe.png"
           alt="Skate Vibe"
           width={100}
           height={100}
-          className="relative pointer-events-none ms-5"
         />
+        <div>© 2025 Skate Vibe. All Rights Reserved</div>
       </div>
-
-      <div>footer</div>
-    </div>
+    </Bounded>
   );
 }
