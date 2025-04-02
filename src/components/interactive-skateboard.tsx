cbd9031f93058d19673e791d251f696fef9ec375
Anas Attoum <anasattoum.12321@gmail.com>
2025-04-02 21:51:28 +0300

    Change 3d Skateboard

--- src/components/interactive-skateboard.tsx
+++ src/components/interactive-skateboard.tsx
@@ -2,19 +2,15 @@
 
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { ContactShadows, Environment, OrbitControls } from '@react-three/drei'
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
 import { Skateboard3D } from "./skateboard-3d";
-import { Model } from "./model";
-
 
 export default function InteractiveSkateboard() {
   return (
     <div className="absolute inset-0 flex justify-center items-center">
       <Canvas
         className="min-h-[60rem] w-full"
-        camera={{ position: [-2, 3, 7]
-          , fov: 80 
-        }}
+        camera={{ position: [40, 25, 50], fov: 90 }}
       >
         <Suspense fallback={<>Loading...</>}>
           <Scene />
@@ -27,23 +23,23 @@ export default function InteractiveSkateboard() {
 function Scene() {
   return (
     <group>
-      <OrbitControls/>
+      <OrbitControls />
       {/* <pointLight position={[1, 1, 1]} intensity={5} /> */}
-      <directionalLight intensity={1} position={[0, 3, 2]}/>
-      <Environment 
-      files="/hdr/warehouse-256.hdr"
-      // preset="forest"
-      // background 
+      <directionalLight intensity={1} position={[0, 3, 2]} />
+      <Environment
+        files="/hdr/warehouse-256.hdr"
+        // preset="forest"
+        // background
       />
 
       {/* <mesh> */}
-        {/* <meshBasicMaterial/> */}
-        {/* <meshStandardMaterial /> */}
-        {/* <boxGeometry /> */}
+      {/* <meshBasicMaterial/> */}
+      {/* <meshStandardMaterial /> */}
+      {/* <boxGeometry /> */}
       {/* </mesh> */}
       {/* <Skateboard3D/> */}
-      <Model/>
-      <ContactShadows opacity={0.6}/>
+      <Skateboard3D />
+      <ContactShadows opacity={0.6} />
     </group>
   );
 }
